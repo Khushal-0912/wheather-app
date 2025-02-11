96ed4ea577c4ce9b1b8ec4733d422b2ff3bea3c4
Khushal Sapkal <sapkalkhushal1111@gmail.com>
2025-02-11 18:02:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Whether from './Component/Whether'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Whether></Whether>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/Whether.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Whether.css"
+
+const Whether = () => {
+  return (
+    <div>Whether
+        
+    </div>
+  )
+}
+
+export default Whether
